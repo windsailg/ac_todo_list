6976890526a5a3e75d55d64556dd07bd0b6c6d25
windsail <windsailg@gmail.com>
2020-08-30 16:46:49 +0800

    add hbs option

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ mongoose.connect('mongodb://localhost/todo-list',
 {useNewUrlParser: true, useUnifiedTopology: true})
 
 const db = mongoose.connection
+const exphbs = require('express-handlebars')
+
+app.engine('hbs', exphbs({
+    defaultLayout: 'main',
+    extname: '.hbs'
+}))
+
+app.set('view engine', 'hbs')
 
 db.on('error', () => {
     console.log('mongoDB error！')
@@ -17,30 +25,12 @@ db.once('open', () => {
 
 
 app.get('/', (req, res) => {
-    res.send('hello world')
-    // res.render('index')
+    // res.send('hello world')
+    res.render('index')
 })
 
 app.listen(port, () => {
     console.log(`Express is running on http://localhost:${port}`)
 })
 
-
-// const generatorRubbishTalk = require('./generator_rubbishtalk')
-// const exphbs = require('express-handlebars')
-// const bodyParser = require('body-parser')
-
-// app.engine('handlebars', exphbs({
-//     defaultLayout: 'main'
-// }))
-// app.set('view engine', 'handlebars')
-// app.use(bodyParser.urlencoded({extended: true}))
-// app.get('/', (req, res) => {
-//     res.render('index')
-// })
-// app.post('/', (req, res) => {
-//     const options = req.body
-//     const rubbishTalk = generatorRubbishTalk(req.body)
-//     res.render('index', {rubbishTalk: rubbishTalk})
-// })
 
