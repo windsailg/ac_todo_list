be3f55214d70a28c38f1b924db81ed0583b73a2d
windsail <windsailg@gmail.com>
2020-09-07 17:02:47 +0800

    add sort method id asc

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ db.once('open', () => {
 app.get('/', (req, res) => {
   todo.find()// 從資料庫找出資料
     .lean()// 轉圜單純JS物件
+    .sort({ _id: 'asc' })// desc
     .then(todos => res.render('index', { todos }))
     .catch(error => console.error(error))
 })
