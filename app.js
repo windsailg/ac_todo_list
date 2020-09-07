872a53eca71b5a7cc2ce10d329f18b99597cf4ab
windsail <windsailg@gmail.com>
2020-09-07 19:23:04 +0800

    edit seeder option

--- app.js
+++ app.js
@@ -1,37 +1,25 @@
 const express = require('express')
 const app = express()
 const port = 3001
-
-const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/todo-list',
-  { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection
 const exphbs = require('express-handlebars')
-const todo = require('./models/todo')
 
 const bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({ extended: true }))
 const methodOverride = require('method-override')
-app.use(methodOverride('_method'))
-
 const routes = require('./routes')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+app.use(routes)
+
+// 建立伺服器連線
+require('./config/mongoose')
+
+// 前端引擎
 app.engine('hbs', exphbs({
   defaultLayout: 'main',
   extname: '.hbs'
 }))
-
 app.set('view engine', 'hbs')
-
-db.on('error', () => {
-  console.log('mongoDB error！')
-})
-db.once('open', () => {
-  console.log('mongoDB connected')
-})
-
-app.use(routes)
 
 app.listen(port, () => {
   console.log(`Express is running on http://localhost:${port}`)
