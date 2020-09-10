4d5c18bb8f94a4747e49184e4723d8e9e8032b75
windsail <windsailg@gmail.com>
2020-09-10 17:33:09 +0800

    modify style

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 const app = express()
 const port = 3001
-const exphbs = require('express-handlebars')
 
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
