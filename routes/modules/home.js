00e5106aea9bd65a8c6cbf15b5685ad43d943027
windsail <windsailg@gmail.com>
2020-09-16 16:23:44 +0800

    edit heroku env

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -2,6 +2,29 @@ const express = require('express')
 const router = express.Router()
 
 const todo = require('../../models/todo')
+router.use(express.static('public'))
+
+const Vue = require('vue')
+const Vuex = require('vuex')
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    count: 0
+  },
+  mutations: {
+    increment (state) {
+      state.count++
+    }
+  }
+})
+
+const test = function () {
+  console.log(store.state.count)
+}
+
+test()
 
 router.get('/', (req, res) => {
   todo.find()// 從資料庫找出資料
